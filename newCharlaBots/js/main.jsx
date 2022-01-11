a221037624ff70398dc342a825e9dd20a5dcdc52
aban4 <aban@umich.edu>
2022-01-11 16:04:43 -0500

    react updates

--- newCharlaBots/js/main.jsx
+++ newCharlaBots/js/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MakeBot from './makeBot';
 
 // This method is only called once
 ReactDOM.render(
 
   // Insert the post component into the DOM
   //TODO: crete a bot component,
+  <MakeBot />,
   document.getElementById('reactEntry'),
+  
 );
---
+++ newCharlaBots/js/makeBot.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MakeBot extends React.Component {
+  /* Display number of image and post owner of a single post */
+
+  constructor(props) {
+    // Initialize mutable state
+    super(props);
+    this.state = {
+     
+    };
+
+  }
+
+  componentDidMount() {
+    // This line automatically assigns this.props.url to the const variable url
+    const { url } = this.props;
+
+
+  }
+
+  
+
+  render() {
+    // This line automatically assigns this.state.imgUrl to the const variable imgUrl
+    // and this.state.owner to the const variable owner
+    
+
+   
+
+    // Render number of post image and post owner
+    return (
+      <div> please </div>
+    );
+  }
+}
+
+
+export default MakeBot;
