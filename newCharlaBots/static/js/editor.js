38db778e2275a616a002e1b8b3a7e2a38f0532d8
aban4 <aban@umich.edu>
2022-02-23 17:58:23 -0500

    more prog

--- newCharlaBots/static/js/editor.js
+++ newCharlaBots/static/js/editor.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-    console.log("start!!!")
     //get end of url to see what bot id we are looking for            
     let botID = document.getElementById("botid").innerHTML;
     let langID = document.getElementById("langid").innerHTML;
@@ -23,11 +22,9 @@ $(document).ready(function () {
 
 });
 
-//translates canonical to user
+//translates canonical to user code
 function translateLineToUser(mapping, line){
     let translatedLine = "";
-
-    console.log("in translated code")
 
     for (j = 0; j < line.length; j++){
         let keyword = "";
@@ -40,17 +37,19 @@ function translateLineToUser(mapping, line){
                 }
                 else{ break;}
             }
-            console.log(keyword);
             let translatedKeyword = mapping[keyword].trim();
-            console.log(translatedKeyword)
+            
+            //if it does not start w "and", if, end, add 4 spaces
             if (!(translatedKeyword.startsWith("and") ||
                 translatedKeyword.startsWith("if") ||
                 translatedKeyword.startsWith("end"))){
-                //if it does not start w "and", if, end, add 4 spaces
                 translatedLine += "    " + translatedKeyword;
             }
             else{
                 translatedLine += translatedKeyword;
+            }
+            if (translatedKeyword.startsWith("end")){
+                translatedLine += "\n";
             }
         }
         //not a keyword
@@ -68,22 +67,19 @@ function translateLineToUser(mapping, line){
 function translateCanonicalCode(mapping, canonicalCode){
     //parse thru code until see {
 
-    console.log("in translate");
-    console.log(canonicalCode);
     let translatedCode = "";
-
     let linesToTranslate = canonicalCode.split("////");
-    console.log(linesToTranslate);
 
     for (let i = 0; i < linesToTranslate.length; i++){//loops thru each line of code
         let translatedLine = translateLineToUser(mapping, linesToTranslate[i]);
         translatedCode += translatedLine +"\n";
     }
-    console.log(translatedCode);
     return translatedCode;
 }
 
-//TODO: what constitutes as an error
+//TODO: alert error if trying to save a bot with error
+//what constitutes as an error?
+//just alert or tell them the error
 function saveBot(){
     let botname = document.getElementById("botname").value;
     let langID = document.getElementById("langid").innerHTML;
@@ -103,27 +99,23 @@ function saveBot(){
             line = translateLineToCanonical(mappings, line);
             canonicalCode += line;
         }
-        //send it back
+        //send the updated code back to database
         updateCanonicalCode(canonicalCode);
     })
-    //translate back to canonical
-    //send back to db
 }
 
-//returns canonical code from of a keyword
+//translate a line from user language to canonical code
 function translateLineToCanonical(mapping, line){
     let mappingKeys = ["ifAny", "languageid", "andNotAny", "ifAll", "andNotAll", 
         "replyLine", "startReply", "endReply", "endIf", "pickRandom", "endPick"]
 
-    //for loop thru mapping keys, check if line starts with any of them
+    //check if line starts with any of the keywords
     for (let i = 0; i< mappingKeys.length; i++){
         if (line.startsWith(mapping[mappingKeys[i]])){
 
             let lastWords = line.slice(mapping[mappingKeys[i]].length);
             let canonical_str = "{" + mappingKeys[i] + "}" + lastWords + "////";
-
             return canonical_str;
-            //length of how much we want to chop of is mapping[mappingKeys[i].length 
         }
     }
     return line;
@@ -132,47 +124,14 @@ function translateLineToCanonical(mapping, line){
 //should not change what the code looks like
 function updateCanonicalCode(canonicalCode){
 
-    //fetch
     let botID = document.getElementById("botid").innerHTML;
     let botName = document.getElementById("botname").value;
-    console.log(botID);
-    console.log(canonicalCode);
     let url = "/updateBot/?botID=" + (botID).toString().trim() + "&botName=" + (botName).toString().trim() + "&canonicalCode=" + canonicalCode;
-    console.log(url)
     fetch(url, {method: 'PATCH'});
 }
 
 
-
-
+//NEXT STEPS: create bot 
+//be able to chat with a bot
+//edge case: i.e. if not happy and not sad
 //TODO: version history 
-
-
-// for (let i = 0; i < canonicalCode.length; i++){
-//     if (canonicalCode[i]== "{"){
-//         let keyword = "";
-//         i++; //don't want to include first { 
-
-//         for (; i< canonicalCode.length; i++){
-//             if (canonicalCode[i] != "}"){
-//                 keyword +=canonicalCode[i];
-//             }
-//             else{ break;}
-//         }
-//         //add mapped keyword in user specified language
-//         let translatedKeyword = mapping[keyword].trim();
-
-//         if (!(translatedKeyword.startsWith("and") ||
-//             translatedKeyword.startsWith("if") ||
-//             translatedKeyword.startsWith("end"))){
-//             //if it does not start w "and", if, end, add 4 spaces
-//             translatedCode += "    " + translatedKeyword;
-//         }
-//         else{
-//             translatedCode += translatedKeyword;
-//         }
-//     }
-//     else{
-//         translatedCode += canonicalCode[i];
-//     }
-// }
